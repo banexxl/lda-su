bf3b46f0c81f94c83a4fd7d751a39675cba2e02e
banexxl <damjanovic.branislav@gmail.com>
2024-02-17 21:20:59 +0100

    ubacena slika na stranicu za projekte

--- src/sections/view/activity-list-view.tsx
+++ src/sections/view/activity-list-view.tsx
@@ -16,6 +16,7 @@ import { useResponsive } from 'src/hooks/use-responsive';
 import { useBoolean } from 'src/hooks/use-boolean';
 import { useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
+import Image from 'src/components/image';
 
 // ----------------------------------------------------------------------
 
@@ -44,10 +45,14 @@ export const ActivitiesView = ({ completedActivities, inProgressActivities, feat
 
 
   return (
-    <Box sx={{ display: 'grid', gridTemplateColumns: mdUp ? '1.7fr 0.8fr' : '1.3fr', gridTemplateRows: '2fr' }}>
-
+    <Box sx={{
+      display: 'grid', gridTemplateColumns: mdUp ? '1.7fr 0.8fr' : '1.3fr',
+      gridTemplateRows: mdUp ? '.5fr .5fr' : '1fr',
+    }}>
+      <Image src='https://lda-su.s3.eu-central-1.amazonaws.com/heroImages/eu+logo+in+eu+m+6b3366db-62ef-4a71-a4f8-04965772ad2f.png'
+        sx={{ gridRow: '1', gridColumn: '1 / span 2', maxHeight: '300px', display: mdUp ? 'block' : 'none' }}
+      />
       <Activities activites={selectedActivites!} loading={loading.value} />
-
       {
         mdUp &&
         <Container
