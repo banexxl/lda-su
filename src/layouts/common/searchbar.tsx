8ff91bc393504e94f852217f946e71fb993bff39
banexxl <damjanovic.branislav@gmail.com>
2024-02-19 23:25:37 +0100

    searchbar pokusaji

--- src/layouts/common/searchbar.tsx
+++ src/layouts/common/searchbar.tsx
@@ -5,7 +5,7 @@ import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
 import { Theme, alpha, styled, SxProps } from '@mui/material/styles';
-
+import { useTheme } from '@mui/material/styles';
 import { useBoolean } from 'src/hooks/use-boolean';
 
 import Iconify from 'src/components/iconify';
@@ -13,8 +13,14 @@ import Iconify from 'src/components/iconify';
 import { HEADER } from '../config-layout';
 import { useState } from 'react';
 import NotFoundPage from 'src/app/not-found';
-import { setStorage } from 'src/hooks/use-local-storage';
+import { getStorage, removeStorage, setStorage } from 'src/hooks/use-local-storage';
 import { useRouter } from 'next/navigation';
+import { Collapse, Paper, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useOffSetTop } from 'src/hooks/use-off-set-top';
+import { useResponsive } from 'src/hooks/use-responsive';
+import { bgBlur } from 'src/theme/css';
 
 // ----------------------------------------------------------------------
 
@@ -47,9 +53,17 @@ type SearchbarProps = {
 export const Searchbar = ({ sx }: SearchbarProps) => {
   const searchOpen = useBoolean();
   const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(false)
   const router = useRouter()
+  const [searchedData, setSearchedData] = useState([])
+  const [openSearchExpander, setOpenSearchExpander] = useState(false)
+  const offset = useOffSetTop();
+  const theme = useTheme();
+  const mdUp = useResponsive('up', 'md');
+
   const handleSearch = async (inputValue: string) => {
 
+    setLoading(true)
     searchOpen.onFalse
 
     try {
@@ -65,25 +79,31 @@ export const Searchbar = ({ sx }: SearchbarProps) => {
           },
         }).then(async (response) => {
 
-          const searchedData = await response.json()
-          console.log(searchedData);
-
-          return searchedData.data.length == 0 ?
+          setSearchedData(await response.json())
+          setLoading(false)
+          searchOpen.onFalse
+          return searchedData.length == 0 ?
             router.push('/pretraga/nema-rezultata')
             :
-            setStorage('searchedData', JSON.stringify(searchedData))
+            setOpenSearchExpander(true)
         }).catch((error) => {
+          setLoading(false)
+          setOpenSearchExpander(false)
+          searchOpen.onFalse
           console.log(error);
         })
       } else {
-        // setLoading(false)
-        // setSearchResults({ message: 'Navedeni termin nije pronadjen!', data: [] })
+        setLoading(false)
+        setOpenSearchExpander(false)
+        searchOpen.onFalse
       }
 
     } catch (error) {
+      setLoading(false)
+      setOpenSearchExpander(false)
+      searchOpen.onFalse
       console.error('Error searching products:', error);
     }
-
   }
 
 
@@ -95,25 +115,44 @@ export const Searchbar = ({ sx }: SearchbarProps) => {
         </IconButton>
 
         <Slide direction="down" in={searchOpen.value} mountOnEnter unmountOnExit>
-          <StyledSearchbar>
-            <Input
-              autoFocus
-              fullWidth
-              disableUnderline
-              placeholder="Search…"
-              startAdornment={
-                <InputAdornment position="start">
-                  <Iconify icon="carbon:search" sx={{ color: 'text.disabled' }} />
-                </InputAdornment>
-              }
-              sx={{ mr: 1, fontWeight: 'fontWeightBold' }}
-              onInput={(e: any) => setInputValue(e.target.value)}
-            />
-            <Button variant="contained" onClick={() => handleSearch(inputValue)} sx={{ marginRight: '20px' }}>
-              Search
-            </Button>
+          <StyledSearchbar sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '90vw', mt: '25px' }}>
+              <Input
+                autoFocus
+                fullWidth
+                disableUnderline
+                placeholder="Search…"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <Iconify icon="carbon:search" sx={{ color: 'text.disabled' }} />
+                  </InputAdornment>
+                }
+                sx={{ mr: 1, fontWeight: 'fontWeightBold' }}
+                onInput={(e: any) => setInputValue(e.target.value)}
+              />
+              <LoadingButton
+                loading={loading}
+                loadingPosition="center"
+                //loadingIndicator='Pretraga...'
+                variant="outlined"
+                onClick={() => handleSearch(inputValue)} sx={{ marginRight: '20px' }}
+              >
+                Pretraga
+              </LoadingButton>
+            </Box>
           </StyledSearchbar>
+
         </Slide>
+
+        <Slide direction="down" in={openSearchExpander} mountOnEnter unmountOnExit>
+          <StyledSearchbar sx={{ display: 'flex', flexDirection: 'column' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '90vw', mt: '80px' }}>
+              <Typography>test</Typography>
+            </Box>
+          </StyledSearchbar>
+
+        </Slide>
+
       </div>
     </ClickAwayListener>
   );
