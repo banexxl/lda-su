13f671ef786e2b435755191be9bd8b110032a3ce
branislav.damjanovic@maxbet.rs <branislav.damjanovic@maxbet.rs>
2024-02-20 13:28:20 +0100

    vracam na 5 projectsummaries

--- src/services/project-services.ts
+++ src/services/project-services.ts
@@ -73,7 +73,7 @@ const projectsServices = () => {
                const db = client.db('LDA_DB');
                const data: any = await db.collection('ProjectSummaries').aggregate([
                     { $match: { status: 'completed' } },  // Filter documents where completed is true
-                    { $sample: { size: 4 } }         // Randomly sample 5 documents
+                    { $sample: { size: 5 } }         // Randomly sample 5 documents
                ]).sort({ projectEndDateTime: -1 }).toArray()
                return data;
           } catch (error: any) {
