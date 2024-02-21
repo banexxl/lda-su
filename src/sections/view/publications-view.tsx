1041e6d4a5d87fcd6146c2ffea2b34ffc0ca49ed
branislav.damjanovic@maxbet.rs <branislav.damjanovic@maxbet.rs>
2024-02-21 15:27:57 +0100

    ubacen grid, samo treba resiti ubacivanje u bazu

--- src/sections/view/publications-view.tsx
+++ src/sections/view/publications-view.tsx
@@ -1,8 +1,7 @@
 "use client"
 import { Box, Divider, Grid, Link, List, ListItem, Typography, useTheme } from "@mui/material"
-import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+// import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import { Publication } from "src/types/publication";
-import Image from "next/image";
 
 type PublicationsViewProps = {
      publications: Publication[];
@@ -10,25 +9,19 @@ type PublicationsViewProps = {
 
 export const PublicationsView = ({ publications }: PublicationsViewProps) => {
 
-     // const extractURLName = (url: string) => {
-     //      const parts = url.split('/');
-     //      return parts[parts.length - 1]; // Get the last part (file name)
-     // };
-
      const theme = useTheme()
 
      return (
           <Box>
                <Divider sx={{ mb: '30px' }} />
                <Typography variant="h3" sx={{ color: theme.palette.text.primary, textAlign: 'center' }}>Publikacije</Typography>
-               <Box sx={{ width: '100vw' }}>
-                    <Grid container spacing={2} sx={{ mt: '20px' }}>
-                         {publications.map((publication: Publication) => (
-                              <Grid item xs={12} xl={6}>
-                                   <ListItem key={publication._id} sx={{ my: '5px' }}>
-                                        {/* <PictureAsPdfIcon sx={{ mr: '5px' }} /> */}
+               <Box sx={{ width: '100vw', my: '50px' }}>
+                    <Grid container spacing={2}>
+                         {publications.map((publication, index) => (
+                              <Grid item xs={6} md={3} key={index}>
+                                   <ListItem sx={{ my: '5px' }}>
                                         <Link href={publication.publicationURL} target="_blank" rel="noopener">
-                                             <Image src={publication.publicationImageURL} alt={"publication image"} width={200} height={200} />
+                                             <img src={publication.publicationImageURL} alt={"publication image"} width={200} height={200} />
                                              <Typography
                                                   sx={{
                                                        fontSize: '.8rem',
@@ -36,7 +29,8 @@ export const PublicationsView = ({ publications }: PublicationsViewProps) => {
                                                        wordBreak: 'break-all',
                                                        maxWidth: '100%',
                                                        textAlign: 'center'
-                                                  }}>
+                                                  }}
+                                             >
                                                   {publication.publicationTitle}
                                              </Typography>
                                         </Link>
