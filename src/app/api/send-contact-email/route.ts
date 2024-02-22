b4e1164b9075f3253263733e2301960109540bf7
branislav.damjanovic@maxbet.rs <branislav.damjanovic@maxbet.rs>
2024-02-22 11:37:12 +0100

    mora ipak ovako da probam

--- src/app/api/send-contact-email/route.ts
+++ src/app/api/send-contact-email/route.ts
@@ -4,8 +4,8 @@ export async function POST(request: Request, response: Response) {
 
      const transporter = nodemailer.createTransport({
           host: process.env.EMAIL_SERVER_HOST,
-          port: 465,
-          secure: true, // true for 465, false for other ports
+          port: 587,
+          secure: false, // true for 465, false for other ports
           auth: {
                user: process.env.EMAIL_SERVER_USER, // generated ethereal user
                pass: process.env.EMAIL_SERVER_PASSWORD, // generated ethereal password
