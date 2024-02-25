fc133bf12a3400d244f40cfe56eb854449334081
banexxl <damjanovic.branislav@gmail.com>
2024-02-25 19:11:14 +0100

    pocinjem sa uputstvom na https://next-intl-docs.vercel.app/

--- src/services/project-services_en.ts
+++ src/services/project-services_en.ts
@@ -1,8 +1,103 @@
 import { Project } from "src/types/project"
 import { MongoClient } from "mongodb"
 import { ObjectId } from "mongodb"
+import { ProjectSummary } from "src/types/projectSummary";
 
-const projectsServices = () => {
+const projectsServices_en = () => {
+
+     const getAllPublications = async () => {
+
+          const client = await MongoClient.connect(process.env.MONGODB_URI!);
+
+          try {
+               const db = client.db('LDA_DB');
+               const data: any = await db.collection('Publications').find({}).toArray()
+               return data;
+          } catch (error: any) {
+               console.log({ message: error.message })
+          } finally {
+               await client.close();
+          }
+     };
+
+     const getAllProjectSummaries = async () => {
+
+          const client = await MongoClient.connect(process.env.MONGODB_URI!);
+
+          try {
+               const db = client.db('LDA_DB');
+               const data: any = await db.collection('ProjectSummaries_en').find({}).sort({ projectEndDateTime: -1 }).toArray()
+               return data;
+          } catch (error: any) {
+               console.log({ message: error.message })
+          } finally {
+               await client.close();
+          }
+     }
+
+     const getInProgressProjectSummaries = async () => {
+
+          const client = await MongoClient.connect(process.env.MONGODB_URI!);
+
+          try {
+               const db = client.db('LDA_DB');
+               const data: any = await db.collection('ProjectSummaries_en').find({ 'status': 'in-progress' }).sort({ projectEndDateTime: -1 }).toArray()
+               return data;
+          } catch (error: any) {
+               console.log({ message: error.message })
+          } finally {
+               await client.close();
+          }
+     };
+
+     const getCompletedProjectSummaries = async () => {
+
+          const client = await MongoClient.connect(process.env.MONGODB_URI!);
+
+          try {
+               const db = client.db('LDA_DB');
+               const data: any = await db.collection('ProjectSummaries_en').find({ 'status': 'completed' }).sort({ projectEndDateTime: -1 }).toArray()
+               return data;
+          } catch (error: any) {
+               console.log({ message: error.message })
+          } finally {
+               await client.close();
+          }
+     };
+
+     const getRandomCompletedProjectSummaries = async () => {
+
+          const client = await MongoClient.connect(process.env.MONGODB_URI!);
+
+          try {
+               const db = client.db('LDA_DB');
+               const data: any = await db.collection('ProjectSummaries_en').aggregate([
+                    { $match: { status: 'completed' } },  // Filter documents where completed is true
+                    { $sample: { size: 5 } }         // Randomly sample 5 documents
+               ]).sort({ projectEndDateTime: -1 }).toArray()
+               return data;
+          } catch (error: any) {
+               console.log({ message: error.message })
+          } finally {
+               await client.close();
+          }
+     };
+
+     const getProjectSummaryByLink = async (link: string) => {
+
+          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
+
+          try {
+               const db = client.db('LDA_DB')
+               let data: ProjectSummary = await db.collection('ProjectSummaries_en').find({ 'projectSummaryURL': link }).toArray()
+               return data
+          } catch (error: any) {
+               console.log({ message: error.message })
+          }
+          finally {
+               await client.close();
+          }
+     }
 
      const getAllProjects = async () => {
 
@@ -10,7 +105,22 @@ const projectsServices = () => {
 
           try {
                const db = client.db('LDA_DB');
-               const data: any = await db.collection('Projects_en').find({}).toArray()
+               const data: any = await db.collection('Projects').find({}).toArray()
+               return data;
+          } catch (error: any) {
+               console.log({ message: error.message })
+          } finally {
+               await client.close();
+          }
+     };
+
+     const getInProgressProjects = async () => {
+
+          const client = await MongoClient.connect(process.env.MONGODB_URI!);
+
+          try {
+               const db = client.db('LDA_DB');
+               const data: any = await db.collection('Projects').find({ 'status': 'in-progress' }).toArray()
                return data;
           } catch (error: any) {
                console.log({ message: error.message })
@@ -25,7 +135,7 @@ const projectsServices = () => {
 
           try {
                const db = client.db('LDA_DB')
-               let data: Project[] = await db.collection('Projects_en').distinct('projectURL')
+               let data: Project[] = await db.collection('Projects').distinct('projectURL')
                return data
           } catch (error: any) {
                console.log({ message: error.message })
@@ -35,12 +145,13 @@ const projectsServices = () => {
           }
      }
 
-     const getProjectByLink = async (link: string) => {
+     const getProjectByLink = async (projectURL: string) => {
+
           const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
 
           try {
                const db = client.db('LDA_DB')
-               let data: Project[] = await db.collection('Projects_en').find({ link: link }).toArray()
+               let data: Project[] = await db.collection('Projects').find({ 'projectURL': projectURL }).toArray()
                return data[0]
           } catch (error: any) {
                console.log({ message: error.message })
@@ -77,24 +188,25 @@ const projectsServices = () => {
           }
      }
 
-     const getRandomHerbalabProjects = async () => {
+     const getSearchTermResults = async (searchTerm: string) => {
 
+          // const searchTermArray = searchTerm.split(" ")
+
+          //const collections = ['Activities', 'Projects', 'ProjectSummaries_en']; mora samo projects jer url-ovi u activities i project summaries nemju 'prefix'
+          const collections = ['Projects'];
           const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
 
           try {
                const db = client.db('LDA_DB')
-               // Get one random document from the mycoll collection.
-               //db.mycoll.aggregate([{ $sample: { size: 1 } }])
-               // Get one random document matching {a: 10} from the mycoll collection.
-               // db.mycoll.aggregate([
-               //      { $match: { a: 10 } },
-               //      { $sample: { size: 1 } }
-               // ])
-               let data: Project[] = await db.collection('Projects')
-                    .find({ "manufacturer": "herbalab" })
-                    .limit(8)
-                    .toArray()
-               return data
+               // const searchResults = await Promise.all(collections.map(async collectionName => {
+               //      const collection = db.collection(collectionName);
+
+               //      // Perform search in the title field
+               return await db.collection('Projects').find({ subTitle: { $regex: searchTerm, $options: 'i' } }).limit(5).toArray();
+               // }));
+               // const combinedResults = searchResults.reduce((acc, curr) => acc.concat(curr), []);
+
+               // return combinedResults
           } catch (error: any) {
                return { message: error.message }
           }
@@ -102,219 +214,20 @@ const projectsServices = () => {
                await client.close();
           }
      }
-
-     const getAllMainCategories = async () => {
-
-          const client = new MongoClient(process.env.MONGODB_URI!);
-
-          try {
-               await client.connect();
-               const db = client.db('LDA_DB');
-               const mainCategories = await db.collection('Projects').distinct('mainCategory')
-               return mainCategories;
-          } catch (error: any) {
-               console.error('Error fetching main categories:', error);
-               return { message: error.message };
-          } finally {
-               await client.close();
-          }
-     }
-
-     const getAllLogos = async () => {
-
-          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
-
-          try {
-               const db = client.db('LDA_DB')
-               let data: Project[] = await db.collection('LogoURLs').find().toArray()
-               return data
-          } catch (error: any) {
-               return { message: error.message }
-          }
-          finally {
-               await client.close();
-          }
-     }
-
-     const getProductById = async (_id: any) => {
-          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
-          try {
-               const db = client.db('LDA_DB')
-               let product: Project = await db.collection('Projects').findOne({ _id: new ObjectId(_id) })
-               return product
-          } catch (error: any) {
-               return { message: error.message }
-          }
-          finally {
-               await client.close();
-          }
-     }
-
-     const getProjectsByManufacturer = async (manufacturer: string) => {
-
-          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
-          try {
-               const db = client.db('LDA_DB')
-               let products: Project[] = await db.collection('Projects')
-                    .find({ "manufacturer": { $regex: `${manufacturer}`, $options: 'i' } })
-                    .limit(2)
-                    .toArray()
-
-               return products
-          } catch (error: any) {
-               return { message: error.message }
-          }
-          finally {
-               await client.close();
-          }
-     }
-
-     const getProjectsByNameAndOrManufacturer = async (searchTerm: any) => {
-
-          const searchTermArray = searchTerm.split(" ")
-
-          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
-          try {
-               const db = client.db('LDA_DB')
-               let products: Project[] = await db.collection('Projects')
-                    .find({
-                         $or: [
-                              { "name": { $regex: `${searchTermArray[0]}`, $options: 'i' } },
-                              { "manufacturer": { $regex: `${searchTermArray[0]}`, $options: 'i' } },
-                              { "name": { $regex: `${searchTermArray[1]}`, $options: 'i' } },
-                              { "manufacturer": { $regex: `${searchTermArray[1]}`, $options: 'i' } },
-                         ]
-                    }
-                    ).toArray()
-
-               return products
-          } catch (error: any) {
-               return { message: error.message }
-          }
-          finally {
-               await client.close();
-          }
-     }
-
-     const getProjectsByDiscount = async () => {
-          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
-          try {
-               const db = client.db('LDA_DB')
-               let products: Project[] = await db.collection('Projects')
-                    .find({ "discount": true })
-                    .limit(10)
-                    .toArray()
-
-               return products
-          } catch (error: any) {
-               return { message: error.message }
-          }
-          finally {
-               await client.close();
-          }
-     }
-
-     const getLimitedProjectsByMainCategory = async (mainCategory: string, loadedParts: number) => {
-
-          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
-
-          try {
-               const db = client.db('LDA_DB')
-               let products: Project[] = await db.collection('Projects')
-                    .find({ mainCategory: `${mainCategory}` })
-                    .skip(12 * (loadedParts - 1)) // Adjust the skip based on loadedParts
-                    .limit(12)
-                    .toArray()
-               return products
-          } catch (error: any) {
-               return { message: error.message }
-          }
-          finally {
-               await client.close();
-          }
-     }
-
-     const getProjectsByMainCategoryMidCategory = async (mainCategory: string, midCategory: string, loadedParts: any) => {
-
-          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
-          try {
-               const db = client.db('LDA_DB')
-               let products: Project[] = await db.collection('Projects').
-                    find({ mainCategory: mainCategory, midCategory: midCategory })
-                    .skip(10 * (loadedParts - 1)) // Adjust the skip based on loadedParts
-                    .limit(10)
-                    .toArray()
-               return products
-          } catch (error: any) {
-               return { message: error.message }
-          }
-          finally {
-               await client.close();
-          }
-     }
-
-     const getProjectsByMainCategoryMidCategorySubCategory = async (mainCategory: string, midCategory: string, subCategory: string, loadedParts: any) => {
-
-          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
-
-          try {
-               const db = client.db('LDA_DB')
-               let products: Project[] = await db.collection('Projects')
-                    .find({ mainCategory: mainCategory, midCategory: midCategory, subCategory: subCategory })
-                    .skip(10 * (loadedParts - 1)) // Adjust the skip based on loadedParts
-                    .limit(10)
-                    .toArray()
-               return products
-          } catch (error: any) {
-               return { message: error.message }
-          }
-          finally {
-               await client.close();
-          }
-     }
-
-     const getAllManufacturers = async () => {
-          const client: any = await MongoClient.connect(process.env.MONGODB_URI!)
-
-          try {
-               await client.connect();
-               const db = client.db('LDA_DB');
-               const productsCollection = db.collection('Projects');
-
-               const manufacturers = await new Promise((resolve, reject) => {
-                    productsCollection.distinct("manufacturer", (error: any, manufacturers: any) => {
-                         if (error) {
-                              reject(error);
-                         } else {
-                              resolve(manufacturers);
-                         }
-                    });
-               });
-               return manufacturers;
-          } catch (error) {
-               return { message: error };
-          } finally {
-               client.close();
-          }
-     }
-
 
      return {
+          getAllPublications,
           getAllProjects,
+          getSearchTermResults,
+          getCompletedProjectSummaries,
+          getRandomCompletedProjectSummaries,
+          getInProgressProjects,
           getProjectByLink,
           getAllProjectLinks,
-          getAllMainCategories,
-          getProductById,
-          getProjectsByNameAndOrManufacturer,
-          getProjectsByManufacturer,
-          getProjectsByDiscount,
-          getLimitedProjectsByMainCategory,
-          getProjectsByMainCategoryMidCategory,
-          getProjectsByMainCategoryMidCategorySubCategory,
-          getAllLogos,
-          getAllManufacturers,
-          getRandomHerbalabProjects,
+          getAllProjectSummaries,
+          getProjectSummaryByLink,
+          getInProgressProjectSummaries
      }
 }
 
-export default projectsServices
+export default projectsServices_en
