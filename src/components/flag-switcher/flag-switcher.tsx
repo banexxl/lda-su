e17da5c963bbcbb5fac4ac3dc2156c78107dc74e
banexxl <damjanovic.branislav@gmail.com>
2024-03-10 09:48:10 +0100

    jos nista :)

--- src/components/flag-switcher/flag-switcher.tsx
+++ src/components/flag-switcher/flag-switcher.tsx
@@ -1,24 +1,49 @@
+'use'
 import { Box, Button } from '@mui/material';
+import { useRouter } from 'next/navigation';
+
 import React, { useState } from 'react';
 import Flag from 'react-world-flags';
+import { SplashScreen } from '../loading-screen';
+import { usePathname } from 'next/navigation';
 
 export const FlagSwitcher = () => {
-     const [currentFlag, setCurrentFlag] = useState('688');
 
-     const switchFlag = () => {
-          setCurrentFlag(currentFlag === '688' ? '826' : '688');
-     };
-     console.log(currentFlag);
+     //688-serbian, 826-english
+     const [currentFlag, setCurrentFlag] = useState('688');
+     const [loading, setLoading] = useState(false); // State to manage loading
+     const router = useRouter();
+     const path = usePathname();
+
+     const switchLanguage = (locale: string) => {
+          //setLoading(true);
+          const pathArray = path.split('/')
+          const currentRoute = pathArray[1]
+          console.log(currentRoute, locale);
+
+          const concatenatedString = pathArray.slice(1).join('/');
+
+          router.push(`/${currentRoute}/${concatenatedString}`);
+          router.push(`/${currentRoute}/${concatenatedString}`);
+          setCurrentFlag('688');
+          router.push('/sr');
+          setLoading(false);
+     }
+
+     if (loading) {
+          return <SplashScreen sx={{ height: '100dvh', width: '100dvw', position: 'absolute', transform: 'translateX(-50%)' }} />;
+     }
 
      return (
-          <Box sx={{ display: 'flex', paddingBottom: '22px' }}>
-               {/* First flag */}
-               <Box style={{ display: 'flex', position: 'absolute', transition: 'opacity 0.5s', opacity: currentFlag === '688' ? 1 : 0 }} onClick={switchFlag}>
-                    <Flag code={'688'} style={{ height: '20px', width: '20px', borderRadius: '10px' }} />
-               </Box>
+          <Box>
+               <Box sx={{ display: 'flex', paddingBottom: '22px' }}>
+                    {/* First flag */}
+                    <Box style={{ display: 'flex', cursor: 'pointer', position: 'absolute', transition: 'opacity 0.5s', opacity: currentFlag === '688' ? 1 : 0 }} onClick={() => switchLanguage('sr')}>
+                         <Flag code={'688'} style={{ height: '20px', width: '20px', borderRadius: '10px' }} />
+                    </Box>
 
-               {/* Second flag */}
-               <Box style={{ display: 'flex', position: 'absolute', transition: 'opacity 0.5s', opacity: currentFlag === '826' ? 1 : 0 }} onClick={switchFlag}>
+                    {/* Second flag */}
+                    <Box style={{ display: 'flex', cursor: 'pointer', position: 'absolute', transition: 'opacity 0.5s', opacity: currentFlag === '826' ? 1 : 0 }} onClick={() => switchLanguage('en')} />
                     <Flag code={'826'} style={{ height: '20px', width: '20px', borderRadius: '10px' }} />
                </Box>
           </Box>
--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -1,124 +1,24 @@
 import Box from '@mui/material/Box';
 
 import { usePathname } from 'src/routes/hooks';
-// import backgroundURL from "../../../public/large.png"
 import { Header } from './header';
 import { Footer } from './footer';
 import { HEADER } from '../config-layout';
-import { Container, Stack } from '@mui/system';
-import { Drawer, IconButton, Typography } from '@mui/material';
-import Logo from 'src/components/logo';
 import { useBoolean } from 'src/hooks/use-boolean';
-import Iconify from 'src/components/iconify';
-import NavBasicMobile from './nav-basic/mobile/nav-basic-mobile';
 
 // ----------------------------------------------------------------------
-
-const pathsOnDark = ['/travel'];
-
-const spacingLayout = [...pathsOnDark, '/'];
-
-const BASIC_NAV_ITEMS = [
-  { title: 'Home', path: '#' },
-  {
-    title: 'Page',
-    path: '#',
-    children: [
-      {
-        title: 'Page 1',
-        path: '#',
-        children: [
-          { title: 'Page 1.1', path: '#' },
-          { title: 'Page 1.2', path: '#' },
-        ],
-      },
-      {
-        title: 'Page 2',
-        path: '#',
-        children: [
-          { title: 'Page 2.1', path: '#' },
-          { title: 'Page 2.2', path: '#' },
-          {
-            title: 'Page 2.3',
-            path: '#',
-            children: [
-              { title: 'Page 2.3.1', path: '#' },
-              { title: 'Page 2.3.2', path: '#' },
-              { title: 'Page 2.3.3', path: '#' },
-            ],
-          },
-        ],
-      },
-      { title: 'Page 3', path: '#' },
-    ],
-  },
-  { title: 'Activity', path: '#' },
-  { title: 'About', path: '#' },
-  { title: 'Contact', path: '#' },
-  { title: 'External', path: 'https://www.google.com/' },
-];
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const MainLayout = ({ children }: Props) => {
-  const pathname = usePathname();
-  const mobileOpen = useBoolean();
-  const actionPage = (arr: string[]) =>
-    arr.some((path) => pathname === path || pathname === `${path}/`);
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: 1 }}>
-      <Header headerOnDark={actionPage(pathsOnDark)} />
-
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          // backgroundImage: `url(${backgroundURL.src})`,
-          // backgroundSize: 'cover',
-          // backgroundRepeat: 'repeat',
-        }}
-      >
-        {!actionPage(spacingLayout) && <Spacing />}
-
-        <Drawer
-          open={mobileOpen.value}
-          onClose={mobileOpen.onFalse}
-          PaperProps={{
-            sx: {
-              width: 260,
-            },
-          }}
-        >
-          <Logo sx={{ m: 2 }} />
-          <NavBasicMobile
-            data={BASIC_NAV_ITEMS}
-            slotProps={{
-              rootItem: {
-                fontSize: 15,
-                fontFamily: (theme) => theme.typography.fontSecondaryFamily,
-              },
-            }}
-          />
-        </Drawer>
-        {children}
-      </Box>
-
+      <Header headerOnDark={true} />
+      {children}
       <Footer />
     </Box>
-  );
-}
-
-// ----------------------------------------------------------------------
-
-const Spacing = () => {
-  return (
-    <Box
-      sx={{
-        height: { xs: HEADER.H_MOBILE, md: HEADER.H_DESKTOP },
-      }}
-    />
   );
 }
