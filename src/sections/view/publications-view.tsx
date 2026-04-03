7df99aeaf7c0ffde8c71152f7d035a69d465f190
banexxl <damjanovic.branislav@gmail.com>
2026-04-03 20:49:32 +0200

    Refactor PublicationsView to improve responsiveness and fix key prop in publication mapping

--- src/sections/view/publications-view.tsx
+++ src/sections/view/publications-view.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Box, Divider, Grid, Link, List, ListItem, Typography, useTheme, Pagination } from "@mui/material"
+import { Box, Divider, Grid, Link, List, ListItem, Typography, useTheme, Pagination, useMediaQuery } from "@mui/material"
 import { useState } from "react";
 // import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import { Publication } from "src/types/publication";
@@ -12,8 +12,8 @@ type PublicationsViewProps = {
 export const PublicationsView = ({ publications }: PublicationsViewProps) => {
      const theme = useTheme();
      const [page, setPage] = useState(1);
-     const itemsPerPage = theme.breakpoints.up('md') ? 8 : 10;
-
+     const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
+     const itemsPerPage = isMdUp ? 8 : 10;
      const totalPublications = publications ? publications.length : 0;
      const pageCount = Math.ceil(totalPublications / itemsPerPage);
 
@@ -34,7 +34,7 @@ export const PublicationsView = ({ publications }: PublicationsViewProps) => {
                          <>
                               <Grid container spacing={2}>
                                    {paginatedPublications.map((publication, index) => (
-                                        <Grid size={{ xs: 6, md: 3 }} key={index}>
+                                        <Grid size={{ xs: 6, md: 3 }} key={publication._id}>
                                              <ListItem sx={{ my: '5px' }}>
                                                   <Link href={publication.publicationURL} target="_blank" rel="noopener">
                                                        <img src={publication.publicationImageURL} alt={"publication image"} width={180} height={200} style={{ borderRadius: '5px' }} />
